1e8a8a4726eb8d6574a3f353698318e317f01e5d
Alejandro Coca <alxmcr@outlook.com>
2016-04-29 17:03:36 -0400

    First commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rails.application.config.session_store :cookie_store, key: '___COUR_RB___30__ROR__ZIPS_INTEGRATION_MONGO_RAILS_COURSERA_session'
