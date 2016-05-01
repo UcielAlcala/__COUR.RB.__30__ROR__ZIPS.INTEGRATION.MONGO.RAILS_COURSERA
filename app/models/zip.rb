9fa3e31115eb62f5df9a47386421c3bc5e892b7f
Alejandro M. Coca <alxmcr@outlook.com>
2016-04-30 21:23:23 -0400

    Create method 'all'

--- app/models/zip.rb
+++ app/models/zip.rb
@@ -8,4 +8,9 @@ class Zip
   def self.collection
     self.mongo_client['zips']
   end
+
+  #Return all documents in zips collection
+  def self.all(prototype={}, sort={:population=>1}, offset=0, limit=100)
+    collection.find.projection({_id:true, city:true, state:true, pop:true})
+  end
 end
